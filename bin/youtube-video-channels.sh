89642681dbf00ecac945b251484aead1c97060d1
Alex Goretoy <alex@goretoy.com>
2023-02-02 16:59:16 -0600

    update videos - 111TRUTH111

--- bin/youtube-video-channels.sh
+++ bin/youtube-video-channels.sh
@@ -10,9 +10,7 @@ for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z
       echo $entry
       name="$(echo "$entry" | awk '{print $1}')"
       cid="$(echo "$entry" | awk '{print $2}')"
-sed 's/^ *//' > ./$cid.txt <<EOL
-$(yt-dlp --get-id https://www.youtube.com/channel/$cid)
-EOL
+      yt-dlp --get-id --force-write-archive --download-archive ./$cid.txt https://www.youtube.com/channel/$cid
       for vid in $(cat ./$cid.txt); do
         echo "$name $cid" > $vid.txt
       done
