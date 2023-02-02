5fef5d03f6e7531e20a500b7731f1130f6d9dabf
Alex Goretoy <alex@goretoy.com>
2023-02-02 16:37:50 -0600

    update videos - 111TRUTH111

--- bin/youtube-video-channels.sh
+++ bin/youtube-video-channels.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# while true; do ./bin/youtube-video-channels.sh; echo 'waiting 2 hours before run youtube-pics.sh';date; sleep 7200; done
+# while true; do ./bin/youtube-video-channels.sh; echo 'waiting 2 hours before run youtube-video-channels.sh';date; sleep 7200; done
 
 for i in 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z; do
   curl -L https://raw.githubusercontent.com/FETVGuide/livestreams/main/$i.txt -o /tmp/$i.txt
