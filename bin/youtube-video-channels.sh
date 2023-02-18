cf7324aa3d3b849a160305bde95f2d8d9ae32498
Alex Goretoy <alex@goretoy.com>
2023-02-18 08:33:37 -0600

    update videos - 111TRUTH111

--- bin/youtube-video-channels.sh
+++ bin/youtube-video-channels.sh
@@ -16,9 +16,8 @@ EOF
       for vid in $(cat ./$cid.txt); do
         echo "$name $cid" > $vid.txt
       done
-      for i in - _ 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do
-        git add $i*;
-      done
+      # find . -name '*' -print0 | xargs -0 git add
+      git add -A
       git commit -am "update videos - $name";
       git push;
       # sleep 1;
